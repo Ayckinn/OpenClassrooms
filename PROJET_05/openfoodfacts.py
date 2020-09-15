04640957f730ec995c5894c5199fa93d1644825e
ayckinn@pm.me <ayckinn@pm.me>
2020-09-15 12:57:04 +0200

    Update file

--- PROJET_05/openfoodfacts.py
+++ PROJET_05/openfoodfacts.py
@@ -4,7 +4,7 @@
 # =            OPEN FOOD FACTS DATABASE HANDLING            =
 # =               OPENCLASSROOMS - PROJECT 05               =
 # =                                                         =
-# =    Use publics datas from OpenFood Facts API            =
+# =        Use publics datas from OpenFood Facts API        =
 # = =========================================================
 
 
@@ -12,7 +12,7 @@ import sys
 import ui_app
 import terminal_app
 
-from tools import logo
+from term import logo
 from tools import constants as cst
 
 
@@ -25,22 +25,22 @@ def main():
 
 			print(""" Choose a view mode :
 
-		 [1]  \033[1;36mTerminal Interface mode\033[0;m
-		 [2]  \033[1;36mGraphical User Interface mode\033[0;m
+ [1]  \033[1;36mTerminal Interface mode\033[0;m
+ [2]  \033[1;36mGraphical User Interface mode\033[0;m
 
-		 [0x] \033[1;36mExit\033[0;m\n""")
+ [x] \033[1;36mExit\033[0;m\n""")
 
 			user_choice = input(cst.CYAN + " >> " + cst.RESET)
 
 			if user_choice == "1":
 				terminal_app.terminal_mode()
 			elif user_choice == "2":
-				ui_app.ui_mode()
-			elif user_choice == "0x":
-				sys.exit(cst.BLUE + "\n Program exit... See you soon ^^\n")
+				ui_app.db_ui_view()
+			elif user_choice == "x":
+				sys.exit(cst.EXIT_MSG)
 
 	except KeyboardInterrupt:
-		sys.exit("\n\n" + cst.RED +" Program killed by user..\n")
+		sys.exit("\n\n" + cst.KILLED_MSG)
 
 
 if __name__ == "__main__":
